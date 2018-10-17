e426011297cf8c8e53a6928761d9590db2322006
Kevin <ketsang12@gmail.com>
2018-10-16 20:17:04 -0400

    added npm module to simply. making it all on 1 page hopefully

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -2,17 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {directToProjects} from '../actions/header-bar';
 import {connect} from 'react-redux';
+import Scroll from 'react-scroll-to-element';
+import '../css/header-bar.css';
 
 export class HeaderBar extends React.Component{
 
   render(){
     return(
-      <div className='links'>
-        <div><button /* onClick={this.props.dispatch()} */>About</button></div>
-        <div><button onClick={() => this.props.dispatch(directToProjects())}>Projects</button></div>
-        <div><button>Contact Me</button></div>
-        {/* <div><Link to={'/projects'}>Projects</Link></div>
-        <div><Link to={'/contact'}>Contact Me</Link></div> */}
+      <div>
+
       </div>
     );
   }
--- src/components/main-page.js
+++ src/components/main-page.js
@@ -1,30 +1,134 @@
 import React from 'react';
 import About from './about';
-import Projects from './projects';
+// import Projects from './projects';
 import Contact from './contact';
 import {connect} from 'react-redux';
 import '../css/main-page.css';
+import {Link} from 'react-router-dom';
+import {directToProjects} from '../actions/header-bar';
+import Scroll from 'react-scroll-to-element';
+import '../css/header-bar.css';
 
 export class MainPage extends React.Component{
-  scrollToView (){
-    if(this.props.projDir === true){
-      window.scrollTo({
-        top: 500,
-        behavior: 'smooth'
-      });
-    }
-    if(this.props.aboutDir){
-      Element.scrollIntoView(<About/>);
-    }
+  constructor(props){
+    super(props);
+    this.myRef = React.createRef();
   }
+  scrollToView (){
+    // if(this.props.projDir === true){
+    //   window.scrollTo({
+    //     top: 500,
+    //     behavior: 'smooth'
+    //   });
+    // }
+    // if(this.props.aboutDir){
+    //   Element.scrollIntoView(<About/>);
+    // }
+    // if(this.props.projDir === true){
+    //   window.scrollTo(0, 500);
+    // }
+  }
+
   render(){
     return(
-      <div className='main-page'>
-        <About/>
-        <Projects/>
-        <Contact/>
-        {this.scrollToView()}
-      </div>);
+      <div>
+        {/*  <div className='links'>
+          <div><button onClick={this.props.dispatch()}>About</button></div>
+          <div><button>Projects</button></div>
+          <div><button>Contact Me</button></div>
+          <div><Link to={'/projects'}>Projects</Link></div>
+          <div><Link to={'/contact'}>Contact Me</Link></div>
+     </div> */}
+        <nav>
+
+          <ul className="clearfix">
+
+            <div className='nav-left'>
+
+              <li className='about'
+                onClick={() => this.props.dispatch(directToProjects())}>
+                <Scroll type='id' element='about'>About</Scroll>
+         
+              </li>
+
+              <li className='projects'
+                onClick={() => this.props.dispatch(directToProjects())}>
+                <Scroll type='id' element='projects' offset={-50}>Projects</Scroll>
+             
+         
+              </li>
+
+            </div>
+
+            <div className='nav-right'>
+              <li className='contact'
+                onClick={() => console.log('yes')}>
+          Contact
+              </li>
+            </div>
+
+          </ul>
+
+        </nav>
+        <div className='main-page'>
+          <About  id='about'/>
+          <div className='projects' id='projects'>
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>  
+            <h2>Project 2</h2>  
+            <p>Filler Text2</p>  
+            <h2>Project 3</h2>  
+            <p>Filler Text3</p>    
+            <h2>Project 1</h2>  
+            <p>Filler Text1</p>    
+          </div>
+          <Contact id='contact'/>
+          {this.scrollToView()}
+        </div>
+      </div>
+    );
   }
 }
 
