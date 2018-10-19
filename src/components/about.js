efb2399203d2e0b06b9d5b5f6abaa8e59c800966
Kevin <ketsang12@gmail.com>
2018-10-19 11:57:34 -0400

    big css changes and formatting

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,8 @@ export default function About (props){
   return(
     <div className='about-me-content' id='about'>
       <h1>Kevin Tsang</h1>
-      <h2>Full Stack Developer</h2>
+      <h3>Full Stack Developer</h3>
+      <h2>About Me</h2>
       <p>My name is Kevin Tsang and I am a full stack developer. 
         Filler text. Lorum Ipsum Dolor. Lorum Ipsum Dolor.
         Lorum Ipsum Dolor. Lorum Ipsum Dolor. Lorum Ipsum Dolor.
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import '../css/projects.css';
+import png from '../pictures/frontpage.png';
 
 export default function Projects (props){
   return(
     <div className='projects-content' id='projects'>
-      <div>
-        <h2>WeatherFM</h2>  
-        <p>React, Redux, MongoDB, NodeJS, Express, Mocha/Chai, Enzyme/Jest </p>
-        <p>A mood-music app to personalize the way a person experiences weather.</p> 
-        
-        <div className='links'>
-          <a href='https://weatherfm-client.herokuapp.com/'>Live</a>
-          <a href='https://github.com/thinkful-ei22/WeatherFM-Client-PurpleCobras'>Client-Side Repository</a>
-          <a href='https://github.com/thinkful-ei22/WeatherFM-Backend-PurpleCobras'>Server-Side Repository</a>
+      <h2>Projects</h2>
+      <div className='content-box'>
+        <h3>WeatherFM</h3>  
+        <div className='content'>
+          <div className='float'>
+            <p>React, Redux, MongoDB, NodeJS, Express, Mocha/Chai, Enzyme/Jest </p>
+            <p>A mood-music app to personalize the way a person experiences weather.</p> 
+            <div className='links'>
+              <a href='https://weatherfm-client.herokuapp.com/'>Live</a>
+              <a href='https://github.com/thinkful-ei22/WeatherFM-Client-PurpleCobras'>Client-Side Repository</a>
+              <a href='https://github.com/thinkful-ei22/WeatherFM-Backend-PurpleCobras'>Server-Side Repository</a>
+            </div>
+          </div>
+          <img className='project-img' src={png} height='300' width='400'></img>
         </div>
       </div>
        
