688529c860e47d35b6d25dc88910c86695cd26e1
Kevin <ketsang12@gmail.com>
2018-10-25 20:56:36 -0400

    localized png pictures

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,15 +2,11 @@ import React from 'react';
 import '../css/contact.css';
 
 export default function Contact (props){
-  const emailImgUrl = 'https://upload.wikimedia.org/wikipedia/'+
-  'commons/thumb/4/45/New_Logo_Gmail.svg/200px-New_Logo_Gmail.svg.png';
-  const linkedInImgUrl = 'https://upload.wikimedia.org/wikipedia/commons/'+
-  'thumb/c/ca/LinkedIn_logo_initials.png/240px-LinkedIn_logo_initials.png';
   return(
     <div className='contact-content' id='contact'>
       <div className='email contact-links'>
         <img 
-          src={emailImgUrl}
+          src={require('../pictures/GmailLogo.png')}
           alt='Gmail Logo'
           height='61.25'
           width='70'>
@@ -35,7 +31,7 @@ export default function Contact (props){
       </div>
       <div className='linkedin contact-links'>
         <img 
-          src={linkedInImgUrl}
+          src={require('../pictures/LinkedInLogo.png')}
           alt='LinkedIn Logo'
           height='61.25'
           width='70'>
