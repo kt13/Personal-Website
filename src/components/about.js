010801d8f97211a76b3e6da21e05609f8ad9609f
Kevin <ketsang12@gmail.com>
2018-11-19 12:03:35 -0500

    about message updated

--- src/components/about.js
+++ src/components/about.js
@@ -8,10 +8,10 @@ export default function About (props){
         I graduated from the University of Maryland - College Park with a BS in 
         Microbiology, looking to make a difference in the world of medicine. 
         However, growing up in the suburbs of Montgomery County, Maryland, I had always looked up to my mother 
-        - a highly successful government project manager. 
+        - an Oracle database administrator turned project manager. 
         Wanting to expand into different horizons, I applied myself into the tech field. 
-        Learning about all these technologies - I found - was extremely for me. 
-        Learning all these languages and building things, I realized, felt amazing. </p>
+        Learning about all these technologies, I realized, is extremely exciting for me. 
+        All these languages and technologies to explore... to delve... a whole new world of knowledge and excitment has opened up. </p>
         
       <p>I am thoroughly hooked.</p>
      
